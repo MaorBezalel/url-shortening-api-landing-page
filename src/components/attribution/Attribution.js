8a07927a1b4eeb17ef134486ce2c877cb21d3c06
Maor <maorbz858@gmail.com>
2023-09-28 23:03:40 +0300

    Fix: Layout for mobile

--- src/components/attribution/Attribution.js
+++ src/components/attribution/Attribution.js
@@ -3,7 +3,7 @@ import '../../styles/css/style.css';
 export default function Attribution() {
     return (
         <div className="attribution">
-            Challenge and Design by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.
+            Challenge and Design by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.<br/>
             Coded by <a href="https://github.com/MaorBezalel" target="_blank" rel="noreferrer">Maor Bezalel</a>.
         </div>
     );
